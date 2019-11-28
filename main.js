df8b67fb57ada9f19b14b67706a3c433dccbef4a
Mark <markeddd19@gmail.com>
2019-11-28 15:48:17 +0100

    updated

--- main.js
+++ main.js
@@ -1,37 +1,45 @@
-let cards = [
-  { number: 1 },
-  { number: 1 },
-  { number: 2 },
-  { number: 2 },
-  { number: 3 },
-  { number: 3 },
-  // { number: 4 },
-  // { number: 4 },
-  // { number: 5 },
-  // { number: 5 },
-  // { number: 6 },
-  // { number: 6 },
-  // { number: 7 },
-  // { number: 7 },
-  // { number: 8 },
-  // { number: 8 },
-  // { number: 9 },
-  // { number: 9 },
-]
+let firstCardFlipped = false;
+let lockBoard = false;
+let count = 0;
+let cardFront;
+let firstCard;
+let secondCard;
+
+const cards = [
+  {number: 1},
+  {number: 1},
+  {number: 2},
+  {number: 2},
+  {number: 3},
+  {number: 3},
+  {number: 4},
+  {number: 4},
+  {number: 5},
+  {number: 5},
+  {number: 6},
+  {number: 6},
+  {number: 7},
+  {number: 7},
+  {number: 8},
+  {number: 8},
+];
+
+const cardFronts = [
+  {image: 'img/card-front-one.jpg'},
+  {image: 'img/card-front-two.jpg'},
+  {image: 'img/card-front-three.jpg'},
+  {image: 'img/card-front-four.jpg'},
+];
 
 const shuffle = (array) => {
   array.sort(() => Math.random() - 0.5);
 }
 
-let cardFlipped = false;
-let countFlipped = 0;
-
 const cardsWrapper = document.querySelector('.cards-wrapper');
-const newGame = document.querySelector('.new-game p');
+const newGame = document.querySelector('.new-game');
 
 const startGame = () => {
   shuffle(cards);
-  newGame.innerHTML = 'New Game';
   if(cardsWrapper.hasChildNodes()) {
     cardsWrapper.innerHTML = '';
     generateCards();
@@ -41,6 +49,7 @@ const startGame = () => {
 }
 
 const generateCards = () => {
+  cardFront = cardFronts[Math.floor(Math.random()*cardFronts.length)];
   cards.forEach(card => {
     const div = document.createElement('div');
     div.className = 'flip-card';
@@ -54,7 +63,7 @@ const createCards = (number) => {
   return (
     `<div class='flip-card-inner'>
       <div class='flip-card-front'>
-        <img src='img/card-front.jpg' alt='Card' data-number='${number}' />
+        <img src=${cardFront.image} alt='Card' data-number='${number}' />
       </div>
       <div class='flip-card-back'>
         <span>${number}</span>
@@ -62,12 +71,6 @@ const createCards = (number) => {
     </div>`
   );
 }
-
-let firstCardFlipped = false;
-let lockBoard = false;
-let firstCard;
-let secondCard;
-let count = 0;
 
 const flipCard = (event) => {
   if (lockBoard) {
@@ -84,19 +87,21 @@ const flipCard = (event) => {
     console.log(event);
 
     firstCard = event.target;
-    firstCard.offsetParent.offsetParent.classList.add('flipped');
+    firstCard.offsetParent.offsetParent.classList.add('flip');
     console.log(firstCard);
     return;
   }
 
   secondCard = event.target;
-  secondCard.offsetParent.offsetParent.classList.add('flipped');
+  secondCard.offsetParent.offsetParent.classList.add('flip');
   lockBoard = true;
+  setTimeout(()=> {
+    checkForMatch();
+  }, 500);
 
-  setTimeout(()=> {checkForMatch(); }, 500);
   console.log('second');
   console.log(secondCard);
- }
+}
 
 const checkForMatch = () => {
   if (firstCard.dataset.number === secondCard.dataset.number) {
@@ -113,12 +118,13 @@ const checkForMatch = () => {
 const disableCards = () => {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
+  resetCards();
   console.log('disabled cards');
 }
 
 const unflipCards = () => {
-  firstCard.offsetParent.offsetParent.classList.remove('flipped');
-  secondCard.offsetParent.offsetParent.classList.remove('flipped');
+  firstCard.offsetParent.offsetParent.classList.remove('flip');
+  secondCard.offsetParent.offsetParent.classList.remove('flip');
   resetCards();
 }
 
@@ -131,8 +137,10 @@ const resetCards = () => {
 }
 
 const checkIfWon = () => {
-  if(count === 3) {
+  if(count === 2) {
     console.log('you won!!!');
+    cardsWrapper.innerHTML = '';
+    cardsWrapper.innerHTML = '<img src="img/winner.gif" alt="You won!">';
     firstCardFlipped = false;
     lockBoard = false;
     firstCard = null;
@@ -142,4 +150,5 @@ const checkIfWon = () => {
   }
 }
 
+window.onload = startGame();
 newGame.addEventListener('click', startGame);
